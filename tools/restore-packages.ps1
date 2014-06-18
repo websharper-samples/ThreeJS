0e33974569e6b8c08818ebc52d8425bbdb8329f8
Anton Tayanovskyy (t0yv0) <anton.tayanovskyy@gmail.com>
2014-06-18 14:37:34 -0400

    Try to use def + premium sources

--- tools/restore-packages.ps1
+++ tools/restore-packages.ps1
@@ -2,4 +2,4 @@
 rm -r packages -errorAction ignore
 set-alias nuget tools/NuGet/NuGet.exe
 nuget sources add -name premium -source http://premium.websharper.com/nuget -userName appveyor@intellifactory.com -password $env:NG_TOKEN -configFile NuGet.config -nonInteractive
-nuget restore -source premium -configFile NuGet.config -nonInteractive
+nuget restore -configFile NuGet.config -nonInteractive
