c6fdc80013873120a11804a45b77f8515bc53a61
Sándor Rakonczai <sandorr@users.noreply.github.com>
2015-04-03 11:55:46 +0200

    Updated gh-pages.ps1

--- tools/gh-pages.ps1
+++ tools/gh-pages.ps1
@@ -3,7 +3,7 @@
 param ([string] $env = "local", [string] $action = "publish")
 
 $msg = 'gh-pages.ps1: build/html -> gh-pages'
-$gitURL = "https://github.com/intellifactory/websharper.threejs.git"
+$gitURL = "https://github.com/intellifactory/websharper.threejs.samples.git"
 
 write-host -foregroundColor "green" "=====> $msg"
 
